a057719c00df8f4a80c66dd442d05fd15df2f8f2
Iván Zaera Avellón <izaera@gmx.es>
2019-09-02 10:30:27 +0200

    [CP] Add a Button widget that imports a CSS file
    This widgets uses the import CSS feature, so we need to add a bundler
    loader to handle the CSS file.

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Button from "./Button";
 
 export default class extends React.Component {
-	render() {
-		return (
-            <div>
-				<div>
-        	        <span className="tag">Portlet Namespace:</span> 
-					<span className="value">{this.props.portletNamespace}</span>
-				</div>
-				<div>
-    	            <span className="tag">Context Path:</span>
-					<span className="value">{this.props.contextPath}</span>
-				</div>
-				<div>
-	                <span className="tag">Portlet Element Id:</span>
-					<span className="value">{this.props.portletElementId}</span>
-				</div>
-				
-			</div>
-		);
-	}	
+  render() {
+    return (
+      <div>
+        <p>
+          <div> A Button from the project:</div>
+          <Button />
+        </p>
+        <div>
+          <span className="tag">Portlet Namespace:</span>
+          <span className="value">{this.props.portletNamespace}</span>
+        </div>
+        <div>
+          <span className="tag">Context Path:</span>
+          <span className="value">{this.props.contextPath}</span>
+        </div>
+        <div>
+          <span className="tag">Portlet Element Id:</span>
+          <span className="value">{this.props.portletElementId}</span>
+        </div>
+      </div>
+    );
+  }
 }
