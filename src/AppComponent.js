f65dd957cf50fd6b23a5c70b821de05c5519b397
Nader Jafari <nader.jafari1988@gmail.com>
2019-09-23 21:14:03 +0330

    Resolve npm start bugs

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -5,10 +5,64 @@ import Button from "./Button";
 import ChartWithState from "./ChartWithState";
 import fileJson from "./file.json";
 
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
+
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [
+        {
+          headerName: "Make",
+          field: "make"
+        },
+        {
+          headerName: "Model",
+          field: "model"
+        },
+        {
+          headerName: "Price",
+          field: "price"
+        }
+      ],
+      rowData: [
+        {
+          make: "Toyota",
+          model: "Celica",
+          price: 35000
+        },
+        {
+          make: "Ford",
+          model: "Mondeo",
+          price: 32000
+        },
+        {
+          make: "Porsche",
+          model: "Boxter",
+          price: 72000
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
+        <div
+          className="ag-theme-balham"
+          style={{
+            height: "500px",
+            width: "600px"
+          }}
+        >
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            rowData={this.state.rowData}
+          ></AgGridReact>
+        </div>
+
         <p>
           <div> A Button from the project:</div>
           <Button />
--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./Button.css";
+// import "./Button.scss";
 
 export default function() {
   return <button className="Button">Press the button!</button>;
